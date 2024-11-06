738579349e6c170aac274f7106c6ffb101807373
Ilya <ilya.starkov03@gmail.com>
2024-11-06 13:05:29 +0300

    Change hit logic

--- Assets/Scripts/Golf/PlowController.cs
+++ Assets/Scripts/Golf/PlowController.cs
@@ -10,20 +10,26 @@ namespace Golf
         [SerializeField] private float angleA = -45f;
         [SerializeField] private float angleB = 45f;
         [SerializeField] private GameObject plow;
+        [SerializeField] private Transform point;  // точка на клюшке для отслеживания направления удара
+        [SerializeField] private float power = 100f;
 
         private Quaternion m_rotationA;
         private Quaternion m_rotationB;
         private bool m_movingToB = false;
         private bool m_isReturning = false;
+        private Vector3 m_lastPointPosition;
+        private Vector3 m_dir;
 
         private void Start()
         {
             m_rotationA = Quaternion.Euler(angleA, plow.transform.rotation.eulerAngles.y, plow.transform.rotation.eulerAngles.z);
             m_rotationB = Quaternion.Euler(angleB, plow.transform.rotation.eulerAngles.y, plow.transform.rotation.eulerAngles.z);
+            m_lastPointPosition = point.position;
         }
 
         private void FixedUpdate()
         {
+            // Расчёт движения клюшки
             if (m_movingToB)
             {
                 MovePlow(m_rotationB, hitSpeed);
@@ -32,18 +38,10 @@ namespace Golf
             {
                 MovePlow(m_rotationA, returnSpeed);
             }
-        }
 
-        public void StartHit()
-        {
-            m_movingToB = true;
-            m_isReturning = false;
-        }
-
-        public void StopHit()
-        {
-            m_movingToB = false;
-            m_isReturning = true;
+            // Обновляем направление удара
+            m_dir = (point.position - m_lastPointPosition).normalized;
+            m_lastPointPosition = point.position;
         }
 
         private void MovePlow(Quaternion targetRotation, float speed)
@@ -61,6 +59,30 @@ namespace Golf
                 else if (m_isReturning)
                 {
                     m_isReturning = false;
+                }
+            }
+        }
+
+        public void StartHit()
+        {
+            m_movingToB = true;
+            m_isReturning = false;
+        }
+
+        public void StopHit()
+        {
+            m_movingToB = false;
+            m_isReturning = true;
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.TryGetComponent<Stone>(out var stone))
+            {
+                Rigidbody stoneRigidbody = other.rigidbody;
+                if (stoneRigidbody != null)
+                {
+                    stoneRigidbody.AddForce(m_dir * power, ForceMode.Impulse);
                 }
             }
         }
